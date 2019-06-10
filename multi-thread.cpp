42966feb979d6947dbd77b5c83467d4f0eb21143
ItsMatt <46201442+MatthijsReyers@users.noreply.github.com>
2019-06-10 17:14:16 +0200

    Create multi-thread.cpp

---
+++ multi-thread.cpp
@@ -0,0 +1,23 @@
+// 18-05-2019 Matthijs Reyers
+// 
+// GCC compiler args:
+// -std=gnu++11
+// 
+// g++ .\multi-thread.cpp -std=gnu++11 -o .\multi-thread.exe
+
+#include <iostream>
+#include <thread>
+
+void hello_world()
+{
+    std::cout << "Hello from thread!\n";
+}
+
+int main()
+{
+    std::thread threadobj1(hello_world);
+    std::thread threadobj2(hello_world);
+    threadobj1.join();
+    threadobj2.join();
+    return 0;
+}
