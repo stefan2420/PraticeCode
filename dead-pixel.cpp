9e4e7ae5cf29a812827b1c4e3781915bd40e0e95
ItsMatt <46201442+MatthijsReyers@users.noreply.github.com>
2019-06-10 16:54:24 +0200

    Update dead-pixel.cpp

--- dead-pixel.cpp
+++ dead-pixel.cpp
@@ -17,24 +17,16 @@ void SpawnPix(COLORREF colour)
     int x = rand() % 1920;
     int y = rand() % 1080;
     SetPixel(GetDC(NULL),x,y,colour);
-    Sleep(350);
 }
 
 int main(int argc, char* argv[])
 {
-    COLORREF pink = RGB(255,165,210);
     COLORREF red = RGB(255,0,0);
-    COLORREF blue = RGB(0,255,0);
-    COLORREF green = RGB(0,0,255);
-    COLORREF white = RGB(255,255,255);
-    COLORREF black = RGB(0,0,0);
+    
     while (true)
     {
         SpawnPix(red);
-        SpawnPix(blue);
-        SpawnPix(green);
-        SpawnPix(white);
-        SpawnPix(black);
+        Sleep(350);
     }
     return 1;
 }
