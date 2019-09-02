63a7b6d9d96b9ea30e42cefe1e2a6c19b12b60a3
MatthijsReyers <matthijscrafter@gmail.com>
2019-09-02 19:23:18 +0200

    updates

--- AsmExercises/helloWorld.asm
+++ AsmExercises/helloWorld.asm
@@ -1,3 +1,9 @@
+; 02-09-2019
+; 
+; Let is be known that this was the first assembly code I've ever writen. 
+; - Matthijs Reyers
+;
+
 ; Define variables/constants in the data section
 SECTION .DATA
 	message db 'Hello world!', 0xa      ; (0xa is a null byte/string terminator)
@@ -10,12 +16,12 @@ SECTION .TEXT
 _start:
 	mov rax, 1              ; system call (1 = write?)
 	mov rdi, 1              ; file descriptor 1 = STDOUT
-	mov rsi, message        ; string to write
-	mov rdx, messageLength  ; length of string to write
+	mov rsi, message 
+	mov rdx, messageLength
 	syscall                 ; call system
 
 	; Terminate program
 	mov rax, 60             ; system call (1 = exit)
-	mov rdi, 0              ; exit with error code 0 (no error)
+	mov rdi, 0              ; exit code 0 (no error)
 	syscall                 ; call system
 
