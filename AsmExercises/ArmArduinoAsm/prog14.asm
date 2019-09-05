d0b2a712a41b4605b8891cb5a44be9d0b0b4d8a9
MatthijsReyers <matthijscrafter@gmail.com>
2019-09-05 11:22:16 +0200

    here

--- AsmExercises/ArmArduinoAsm/prog14.asm
+++ AsmExercises/ArmArduinoAsm/prog14.asm
@@ -8,14 +8,14 @@
 ; 
 ; First few notes of Beethoven's 'Fur Elise':
 ; 
-; E  - 329.63 hz - (16000000 - 329.63 * 2) / 2 = 7999670 (<= needed instructions between turning on and off).
-; D# - 311.13 hz - (16000000 - 311.13 * 2) / 2 = 7999689
+; E  - 329.63 hz - 
+; D# - 311.13 hz - 
 ; E  - 329.63 hz
 ; D# - 311.13 hz
-; B  - 246.94 hz - (16000000 - 246.94 * 2) / 2 = 7999753
-; D  - 293.66 hz - (16000000 - 293.66 * 2) / 2 = 7999706
-; C  - 261.63 hz - (16000000 - 261.63 * 2) / 2 = 7999738
-; A  - 220.00 hz - (16000000 - 220.00 * 2) / 2 =  7999780
+; B  - 246.94 hz - 
+; D  - 293.66 hz - 
+; C  - 261.63 hz - 
+; A  - 220.00 hz - (16000000-2) / (220 * 2) = 36364
 ;
 ; NOTE: We're doubling the frequenty of the notes (effectivily moving the 
 ; notes up one octave) to make the mellody more clear and to create a 
@@ -35,7 +35,7 @@
 
 ; Variables for the low/high state of the pin.
 ldi     r23, 0
-ldi     r24, $10
+ldi     r24, $20
 
 ; Variable for how long to play the note.
 ldi     r25, $A
@@ -52,29 +52,29 @@ main:
     ; a custom Python script that helps calculate how many 
     ; loops to use to occelate at a certain frequenty.
 
-    ldi     r16, 122        ; Play an E (329 hz)
-    ldi     r17, 119
-    ldi     r18, 183
+    ldi     r16, 7          ; Play an E (329 hz)
+    ldi     r17, 216
+    ldi     r18, 253
     call    playNote 
 
-    ldi     r16, 80         ; Play a D# (311 hz)
-    ldi     r17, 173
-    ldi     r18, 192
+    ldi     r16, 7          ; Play a D# (311 hz)
+    ldi     r17, 236
+    ldi     r18, 210
     call    playNote
 
-    ldi     r16, 122        ; Play an E (329 hz)
-    ldi     r17, 119
-    ldi     r18, 183
+    ldi     r16, 7          ; Play an E (329 hz)
+    ldi     r17, 216
+    ldi     r18, 253
     call    playNote 
 
-    ldi     r16, 80         ; Play a D# (311 hz)
-    ldi     r17, 173
-    ldi     r18, 192
+    ldi     r16, 7          ; Play a D# (311 hz)
+    ldi     r17, 236
+    ldi     r18, 210
     call    playNote
 
-    ldi     r16, 128        ; Play a B  (247 hz)
-    ldi     r17, 252
-    ldi     r18, 82
+    ldi     r16, 9        ; Play a B  (247 hz)
+    ldi     r17, 234
+    ldi     r18, 236
     call    playNote
 
     ldi     r16, 158        ; Play a D (294 hz)
@@ -128,23 +128,27 @@ playNote:
 ; based on values of TEMP TEMP)
 ; -----------------------------------------------------------
 sleep:
-    ldi     r29, 0          ; Three loops that will ensure
+
+    ldi         r29, 0      ; Two loops that will ensure
     loop3:                  ; that the note is played for a
-    inc     r29             ; sufficient amount of time.
-    ldi     r30, 0          ; 
-    loop4:                  ;
-    inc     r30             ;
-    ldi     r31, 0          ; 
-    loop5:                  ; 
-    inc     r31             ; 
+        inc     r29         ; sufficient amount of time.
+        ldi     r30, 0      
+        loop4:              
+            inc r30     
 
-        nop                 ; nop operation to waste instruction cycles.
+            nop             ; Waste time with a nop
 
-    cp r31, r18             ; Bottom of the loop bodies, this 
-    brne loop5              ; code will restart the loops if
-    cp r30, r17             ; necessary.
-    brne loop4              ;
-    cp r29, r16             ;
-    brne loop3              ;
+            cp r30, r17         ; end of loop 4
+            brne loop4          
+
+        cp r29, r16             ; end of loop 3
+        brne loop3              
+
+    ; second loop to get the last few instructions.
+    ldi     r31, 0   
+    loop5:                 
+        inc     r31          
+        cp r31, r18             ; Bottom of the loop bodies, this 
+        brne loop5              ; code will restart the loops if
 
     ret                     ; return from subroutine
